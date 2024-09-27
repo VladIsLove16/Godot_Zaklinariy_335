821451738af6c025f91db7fb79f62ca422592f5e
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-09-27 18:58:45 +0300

    UI changes

--- GodotVersion/Scripts/SpawnChances.cs
+++ GodotVersion/Scripts/SpawnChances.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 public static class SpawnChances
 {
-    public static Dictionary<GhostType, int> values = new Dictionary<GhostType, int>()
-    {
-        [GhostType.Swipe] = 0,
-        [GhostType.DoubleSwipe] = 0,
-        [GhostType.DoubleTap] = 0,
-        [GhostType.Skip] = 1500,
-        [GhostType.Spin] = 0,
+	public static Dictionary<GhostType, int> values = new Dictionary<GhostType, int>()
+	{
+		[GhostType.Swipe] = 0,
+		[GhostType.DoubleSwipe] = 0,
+		[GhostType.DoubleTap] = 0,
+		[GhostType.Skip] = 1500,
+		[GhostType.Spin] = 0,
 
-    };
+	};
 }
